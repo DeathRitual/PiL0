0a0c79bb0b885ecd65b5a94aaf6577565096eb3e
DeathRitual <philipwiesner@gmail.com>
2013-02-11 17:23:24 +0100

    Update lexer.c

--- lexer.c
+++ lexer.c
@@ -14,9 +14,65 @@
 
 void main(void) {
 
-  struct key_array reserved = init_ReservedKeys();
-  
-  printf("Wort: %s, ID: %d\n", reserved.resKeys[15].w, reserved.resKeys[15].ID);
+keyword *reserved = init_ReservedKeys();
+  list token_stream;
+	l_init(&token_stream);
+
+	int key_NUM = get_keyNUM(reserved, "BEGIN");
+	printf("%s, %d", reserved[key_NUM].w, reserved[key_NUM].ID);
+	l_append(&token_stream, "BEGIN", (int *)256);
+	//l_append(&token_stream, reserved[key_NUM].w, (int *)reserved[key_NUM].ID);
+
+	char string[50] = "BEGIN\n\tVAR i = 234 + a;\nEND";
+	printf("\n%s\n\n", string);
+
+	int i;
+	for (i = 0; i < strlen(string); i++) {
+	  while (string[i] < 33)
+		  i++;
+
+	  if (string[i] == 61 && string[i + 1] == 61) {
+		  printf("Token: ==");
+		  i++;
+	  }
+	  if (string[i] == 60 && string[i + 1] == 61) {
+		  printf("Token: <=");
+		  i++;
+	  }
+	  if (string[i] == 62 && string[i + 1] == 61) {
+		  printf("Token: >=");
+		  i++;
+	  }
+	  if (string[i] == 33 && string[i + 1] == 61) {
+		  printf("Token: !=");
+		  i++;
+	  }
+
+	  char w[20] = "";
+	  int j = 0;
+	  if (isalpha(string[i])) {
+		  do {
+			  w[j] = string[i];
+			  j++;
+			  i++;
+		  } while (isalnum(string[i]));
+		  //int key_NUM = get_keyNUM(reserved, w);
+		  //if (key_NUM > 0) l_append(&token_stream, reserved[key_NUM].w, (int *)reserved[key_NUM].ID);
+		  //else l_append(&token_stream, w, (int *)IDENTIFIER);
+	  }
+
+	  if (isdigit(string[i])) {
+		  do {
+			  w[j] = string[i];
+			  j++;
+			  i++;
+		  } while (isdigit(string[i]));
+		  //l_append(&token_stream, w, (int *)NUM);
+	  }
+
+	  if (strlen(w) > 0) i--;
+	  else printf("test");//l_append(&token_stream, &string[i], (int *)NUM);
+	}
 
 
 } 
